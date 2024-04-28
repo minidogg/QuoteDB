85e74649c84d7e2a1f9e1703bfb7ddde1f3ec6da
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-04-27 19:48:01 -0500

    nearly done?

--- main/commands/quotes.js
+++ main/commands/quotes.js
@@ -5,19 +5,52 @@ const db = require("../db.js")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('quotes')
-		.setDescription('Replies with last 20 quotes.'),
+		.setDescription('Replies with last 20 quotes.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('user')
+                .setDescription('Get quotes from a specific user.')
+                .addUserOption(option => option.setName('user').setDescription('The user id/mention').setRequired(true)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('last')
+                .setDescription('Gets last 20 quotes')
+                /*.addUserOption(option => option.setName('user').setDescription('The user id/mention'))*/),
         /**
          * 
          * @param {Interaction} interaction 
          */
 	async execute(interaction) {
-        let quotes = db.getQuotes().map(e=>e.quote).join("\n")
+        switch(interaction.options._subcommand){
+            case("last"):
+                var quotes = db.getQuotes().map(e=>e.quote).join("\n")
 
-        await interaction.reply({embeds:[lazyEmbed({
-            "title":"Quotes",
-            "message":`Last 20 quotes: \n\n${quotes}`
-        })],ephemeral:
-            !(interaction.channel.name.includes("command")||interaction.channel.name.includes("bot"))
-        });
+                await interaction.reply({embeds:[lazyEmbed({
+                    "title":"Quotes",
+                    "message":`Last 20 quotes: \n\n${quotes}`
+                })],ephemeral:
+                    !(interaction.channel.name.includes("command")||interaction.channel.name.includes("bot"))
+                });
+                break;
+            case("user"):
+                var id = parseFloat(interaction.options._hoistedOptions[0].value)
+                if(Number.isNaN(id)){
+                    interaction.reply("Invalid user/userID!")
+                    return
+                }
+                var quotes = db.getQuotesOf(id).map(e=>e.quote).join("\n")
+
+                await interaction.reply({embeds:[lazyEmbed({
+                    "title":"Quotes",
+                    "message":`Last 20 quotes: \n\n${quotes}`
+                })],ephemeral:
+                    !(interaction.channel.name.includes("command")||interaction.channel.name.includes("bot"))
+                });
+                break;
+            default:
+                interaction.reply("Subcommand doesn't exist?")
+                break;
+        }
+
 	},
 };
