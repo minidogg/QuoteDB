1662ff4f4ea04bc447b9b4a246ae24bbe275b4f7
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-04-28 15:29:13 -0500

    Update discord.js

--- main/commands/discord.js
+++ main/commands/discord.js
@@ -1,1 +1,26 @@
-console.log
+const { SlashCommandBuilder,Interaction,ButtonBuilder,ActionRowBuilder,ButtonStyle } = require('discord.js');
+
+const deleteButton = new ButtonBuilder()
+.setCustomId('delete')
+.setLabel('Delete')
+.setStyle(ButtonStyle.Danger);
+const deleteRow = new ActionRowBuilder()
+.addComponents(deleteButton);
+
+const lazyEmbed = require("../lazyEmbed.js")
+
+module.exports = {
+        data: new SlashCommandBuilder()
+                .setName('discord')
+                .setDescription('replies with discord invite'),
+        /**
+         * 
+         * @param {Interaction} interaction 
+         */
+        async execute(interaction) {
+                await interaction.reply({embeds:[lazyEmbed({
+            "title":"Discord",
+            "message":"[Discord](https://disboard.org/server/1234184478335828012)","color":"#FF00FF"
+        })],ephemeral:interaction.channel.name.includes("command")||interaction.channel.name.includes("bot")});
+        },
+};
