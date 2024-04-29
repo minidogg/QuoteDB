7dd18d0d6b3b38b86a6303565d60f6ba0b5e3eab
AbstractMelon <129030233+AbstractMelon@users.noreply.github.com>
2024-04-29 16:04:42 -0600

    added help command

---
+++ main/commands/help.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder,Interaction,ButtonBuilder,ActionRowBuilder,ButtonStyle } = require('discord.js');
+
+const deleteButton = new ButtonBuilder()
+.setCustomId('delete')
+.setLabel('Delete')
+.setStyle(ButtonStyle.Danger);
+const deleteRow = new ActionRowBuilder()
+.addComponents(deleteButton);
+
+const lazyEmbed = require("../lazyEmbed.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('help')
+		.setDescription('Teaches you how to use the bot'),
+        /**
+         * 
+         * @param {Interaction} interaction 
+         */
+	async execute(interaction) {
+		await interaction.reply({embeds:[lazyEmbed({
+            "title":"Github Repo",
+            "message":"[Repo](https://github.com/ReallyBadDeveloper/QuoteDB)",
+            "color":"#000000"
+        })],ephemeral:interaction.channel.name.includes("command")||interaction.channel.name.includes("bot")});
+	},
+};
