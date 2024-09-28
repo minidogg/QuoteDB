01d262b7f80d8e1f9df09dfd070b15f211cec208
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-09-28 15:29:44 -0500

    DB Structure

--- main/db.js
+++ main/db.js
@@ -1,2 +1,15 @@
 const fs = require("fs");
-fs
+const path = require("path")
+
+// Utility function for making directories if they don't already exist
+function tryMakeDir(dirPath){
+    if(!fs.existsSync(dirPath))fs.mkdirSync(dirPath)
+}
+
+// Setup the folder structure
+const dbPath = path.resolve("../db")
+tryMakeDir(dbPath)
+const usersPath = path.join(dbPath, "users")
+tryMakeDir(usersPath)
+const guildsPath = path.join(dbPath, "guilds")
+tryMakeDir(guildsPath)
